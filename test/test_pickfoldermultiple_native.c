ec07834d7dd5a83feacb71b60b8f2d27b6b731bd
Bernard Teo <btzy1996@hotmail.com>
2024-06-23 18:06:20 +0800

    New feature: Implement PickFolderMultiple (#135)
    We have OpenFile, OpenFileMultiple, and PickFolder. It's only reasonable to also add PickFolderMultiple, since all backends support it. It does the expected thing on all backends.

---
+++ test/test_pickfoldermultiple_native.c
@@ -0,0 +1,52 @@
+#define NFD_NATIVE
+#include <nfd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* this test should compile on all supported platforms */
+
+int main(void) {
+    // initialize NFD
+    // either call NFD_Init at the start of your program and NFD_Quit at the end of your program,
+    // or before/after every time you want to show a file dialog.
+    NFD_Init();
+
+    const nfdpathset_t* outPaths;
+
+    // show the dialog
+    nfdresult_t result = NFD_PickFolderMultiple(&outPaths, NULL);
+
+    if (result == NFD_OKAY) {
+        puts("Success!");
+
+        nfdpathsetsize_t numPaths;
+        NFD_PathSet_GetCount(outPaths, &numPaths);
+
+        nfdpathsetsize_t i;
+        for (i = 0; i < numPaths; ++i) {
+            nfdchar_t* path;
+            NFD_PathSet_GetPath(outPaths, i, &path);
+#ifdef _WIN32
+            wprintf(L"Path %i: %s\n", (int)i, path);
+#else
+            printf("Path %i: %s\n", (int)i, path);
+#endif
+
+            // remember to free the pathset path with NFD_PathSet_FreePath (not NFD_FreePath!)
+            NFD_PathSet_FreePath(path);
+        }
+
+        // remember to free the pathset memory (since NFD_OKAY is returned)
+        NFD_PathSet_Free(outPaths);
+    } else if (result == NFD_CANCEL) {
+        puts("User pressed cancel.");
+    } else {
+        printf("Error: %s\n", NFD_GetError());
+    }
+
+    // Quit NFD
+    NFD_Quit();
+
+    return 0;
+}
